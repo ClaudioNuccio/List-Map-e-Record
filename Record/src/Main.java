3e7fe88858b5482f018c2083addefa863ea8229a
Claudio Nuccio <cnuccio00@gmail.com>
2023-11-14 12:43:47 +0100

    Added Record

---
+++ Record/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Point point = new Point(6,5);
+        System.out.println("Your point x is " + point.x());
+        System.out.println("Your point y is " + point.y());
+        System.out.println(point);
+    }
+}
