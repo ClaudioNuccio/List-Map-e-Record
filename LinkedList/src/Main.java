43fd0e4f92b65ce70b86769cd546ed4cbebe13c7
Claudio Nuccio <cnuccio00@gmail.com>
2023-11-14 11:27:32 +0100

    Added LinkedList

---
+++ LinkedList/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.LinkedList;
+
+public class Main{
+    public static void main (String [] args){
+        LinkedList<Fruit> fruits = new LinkedList<>();
+        Fruit fruit1 = new Fruit("Apple");
+        Fruit fruit2 = new Fruit("Banana");
+        Fruit fruit3 = new Fruit("Peach");
+        fruits.add(fruit1);
+        fruits.add(fruit2);
+        fruits.add(fruit3);
+        System.out.println(fruits);
+        Fruit fruit4 = new Fruit("Pear");
+        Fruit fruit5 = new Fruit("Cherry");
+        fruits.addFirst(fruit4);
+        fruits.addLast(fruit5);
+        System.out.println(fruits);
+    }
+}
