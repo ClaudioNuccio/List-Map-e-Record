c5ee856f83055b730532c69383c2673f2103f935
Claudio Nuccio <cnuccio00@gmail.com>
2023-11-18 13:08:35 +0100

    Fixed Testing_5

--- Testing_5/src/Main.java
+++ Testing_5/src/Main.java
@@ -3,12 +3,14 @@ import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
 public class Main {
-    public String exercise (OffsetDateTime data, int year, int month, int day){
+    public static String exercise (OffsetDateTime data, int year, int month, int day){
         String addedTime = data.plusYears(year).minusMonths(month).plusDays(day).format(DateTimeFormatter.ofPattern("dd MMMM yyyy", Locale.ITALY));
+        System.out.println(addedTime);
        return addedTime;
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        OffsetDateTime sample = OffsetDateTime.parse("2023-03-01T13:00:00Z");
+        exercise(sample,1,1,7);
     }
 }
