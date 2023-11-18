8bcfa0f5d4c7de1a08a28ce84ebbde85c32cce33
Claudio Nuccio <cnuccio00@gmail.com>
2023-11-18 13:13:11 +0100

    Fixed Testing_3

--- Testing_3/src/Main.java
+++ Testing_3/src/Main.java
@@ -5,12 +5,14 @@ import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 public class Main {
-    public String dateOfItaly(OffsetDateTime date){
+    public static String dateOfItaly(OffsetDateTime date){
         String result = date.format(DateTimeFormatter.ofPattern("dd MMMM yyyy", Locale.ITALY));
-        System.out.println(result);
+        System.out.println("The date in italy is " + result);
         return result;
     }
 
     public static void main(String[] args) {
+        OffsetDateTime sample = OffsetDateTime.parse("2023-03-01T13:00:00Z");
+        dateOfItaly(sample);
     }
     }
