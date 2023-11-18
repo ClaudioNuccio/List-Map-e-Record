efe40c00cb87c424b0a2c04d20541927dceb4d1d
Claudio Nuccio <cnuccio00@gmail.com>
2023-11-18 13:15:24 +0100

    Fixed testing 2

--- Testing_2/src/Main.java
+++ Testing_2/src/Main.java
@@ -3,23 +3,25 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 
 public class Main {
-    public  String printFull (OffsetDateTime string){
+    public static String printFull (OffsetDateTime string){
         String printFull = string.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL));
-        System.out.println(printFull);
+        System.out.println("The date in the FULL format is " + printFull);
        return printFull;
     }
-    public  String printMedium (OffsetDateTime string){
+    public static String printMedium (OffsetDateTime string){
         String printMedium = string.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM));
-        System.out.println(printMedium);
+        System.out.println("The date in the MEDIUM format is " + printMedium);
         return printMedium;
-    }public  String printShort (OffsetDateTime string){
+    }public static String printShort (OffsetDateTime string){
         String printShort = string.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT));
-        System.out.println(printShort);
+        System.out.println("The date in the SHORT format is " + printShort);
         return printShort;
     }
 
     public static void main(String[] args) {
-
+        OffsetDateTime sample = OffsetDateTime.parse("2002-03-01T13:00:00Z");
+        printFull(sample);
+        printMedium(sample);
+        printShort(sample);
     }
-
 }
