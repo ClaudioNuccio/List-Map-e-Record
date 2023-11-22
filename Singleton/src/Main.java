024ea946eae06c5f39c4b312d5572761b8a2295e
Claudio Nuccio <cnuccio00@gmail.com>
2023-11-22 16:45:46 +0100

    fixed

--- Singleton/src/Main.java
+++ Singleton/src/Main.java
@@ -10,17 +10,19 @@ public class Main {
 
     public static void main(String[] args) {
         User user1 = getInstance();
-        System.out.println("User 1 " + user1);
+        user1.printInfo();
 
         user1.setAge(25);
         user1.setName("Marcello");
-        System.out.println("Modified user1 " + user1);
+        System.out.println("Modified user1 ");
+       user1.printInfo();
 
         User user2 = getInstance();
 
         System.out.println("User2 " + user2);
         user2.setAge(32);
         user2.setName("Francesco");
-        System.out.println("Modified user 2 " + user2);
+        System.out.println("Modified user2 " );
+        user2.printInfo();
     }
 }
--- Singleton/src/User.java
+++ Singleton/src/User.java
@@ -1,19 +1,10 @@
 public class User {
     private String name;
-
-    private static User instance;
     private int age;
+    private static User instance;
 
     public String getName() {
         return name;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "name='" + name + '\'' +
-                ", age=" + age +
-                '}';
     }
 
     public void setName(String name) {
@@ -28,14 +19,22 @@ public class User {
         this.age = age;
     }
 
+
     public User(String name, int age) {
         this.name = name;
         this.age = age;
     }
 
     public void printInfo() {
-        System.out.println(name);
-        System.out.println(age);
+        System.out.println("name " + name);
+        System.out.println("age " + age);
     }
 
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
