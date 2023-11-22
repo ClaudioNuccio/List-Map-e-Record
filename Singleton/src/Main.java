7ebc14c1125ea5351d53d64f40ef002d4c4e979c
Claudio Nuccio <cnuccio00@gmail.com>
2023-11-22 16:20:55 +0100

    Added Singleton

---
+++ Singleton/src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    private static User instance;
+    private static User getInstance(){
+        if (instance ==  null){
+            instance = new User("default", 0);
+        }
+        return instance;
+    }
+
+    public static void main(String[] args) {
+       User user1 = getInstance();
+        System.out.println("User 1 " + user1);
+
+        user1.setAge(25);
+        user1.setName("Marcello");
+        System.out.println("Modified user1 " + user1);
+
+        User user2 = getInstance();
+
+        System.out.println("User2 " + user2);
+        user2.setAge(32);
+        user2.setName("Francesco");
+        System.out.println("Modified user 2 " + user2);
+    }
+}
---
+++ Singleton/src/User.java
@@ -0,0 +1,43 @@
+public class User {
+    private String name;
+
+    private static User instance;
+    private int age;
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void printInfo (){
+        System.out.println(name);
+        System.out.println(age);
+    }
+
+}
