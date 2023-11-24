beb09f43321440ba276496bcb0caa678ddfb2fa3
Claudio Nuccio <cnuccio00@gmail.com>
2023-11-24 11:42:20 +0100

    fixed BigDecimal

--- BigDecimal/src/Main.java
+++ BigDecimal/src/Main.java
@@ -1,4 +1,5 @@
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,37 +13,34 @@ bigDecimalCalculator(Operations.ADDITION,number1,number2);
         bigDecimalCalculator(Operations.MIN,number1,number2);
     }
 
-    public static BigDecimal bigDecimalCalculator (Operations operations, BigDecimal a, BigDecimal b){
-        BigDecimal result = null;
-        switch (operations){
-            case ADDITION :
+    public static void bigDecimalCalculator (Operations operations, BigDecimal a, BigDecimal b){
+            BigDecimal result ;
+        switch (operations) {
+            case ADDITION -> {
                 result = a.add(b);
                 System.out.println(result);
-
-            break;
-            case SUBTRACTION :
+            }
+            case SUBTRACTION -> {
                 result = a.subtract(b);
                 System.out.println(result);
-            break;
-            case MULTIPLICATION :
+            }
+            case MULTIPLICATION -> {
                 result = a.multiply(b);
                 System.out.println(result);
-            break;
-            case DIVIDE :
-                result = a.divide(b);
+            }
+            case DIVIDE -> {
+                result = a.divide(b, RoundingMode.CEILING);
                 System.out.println(result);
-            break;
-            case MAX :
+            }
+            case MAX -> {
                 result = a.max(b);
                 System.out.println(result);
-            break;
-            case MIN :
+            }
+            case MIN -> {
                 result = a.min(b);
                 System.out.println(result);
-                break;
-            default:
-                System.out.println("Wrong input");
+            }
+            default -> System.out.println("Wrong input");
         }
-        return result;
     }
 }
