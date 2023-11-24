6ce746c394a5f327f5cc6b2ced67ee0f2c29fa92
Claudio Nuccio <cnuccio00@gmail.com>
2023-11-24 11:49:21 +0100

    fixed BigDecimal Formatting

--- BigDecimal/src/Main.java
+++ BigDecimal/src/Main.java
@@ -18,27 +18,27 @@ bigDecimalCalculator(Operations.ADDITION,number1,number2);
         switch (operations) {
             case ADDITION -> {
                 result = a.add(b);
-                System.out.println(result);
+                System.out.println("Result of addition is  " + result);
             }
             case SUBTRACTION -> {
                 result = a.subtract(b);
-                System.out.println(result);
+                System.out.println("Result of subtraction is  " + result);
             }
             case MULTIPLICATION -> {
                 result = a.multiply(b);
-                System.out.println(result);
+                System.out.println("Result of multiplication is  " + result);
             }
             case DIVIDE -> {
                 result = a.divide(b, RoundingMode.CEILING);
-                System.out.println(result);
+                System.out.println("Result of division is  " + result);
             }
             case MAX -> {
                 result = a.max(b);
-                System.out.println(result);
+                System.out.println("The max number between A and B is  " + result);
             }
             case MIN -> {
                 result = a.min(b);
-                System.out.println(result);
+                System.out.println("The min number between A and B is  " + result);
             }
             default -> System.out.println("Wrong input");
         }
